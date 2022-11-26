42e0d385a4bed6bd22b9b2f9fa84205009bf5f3a
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2022-11-26 17:12:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const superbowlWin = (record) =>{
+    let broncosYear = record.find(item => item.result === 'W')
+    if(broncosYear !== undefined){
+        return broncosYear.year
+    }
+    else {
+        return undefined
+    }
+}
+
+superbowlWin(record)
